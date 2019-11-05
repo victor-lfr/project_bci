626310806cb5007f3f5beab832ccabc603a2148f
TomCG <thomas.cop--gaillot@edu.ece.fr>
2019-11-05 14:04:54 +0100

    listener stores data in a vector

--- matlab/test.m
+++ matlab/test.m
@@ -1,14 +1,33 @@
-fc = [7.5,11,13.5];
+fc = 7.5; %[7.5,11,13.5];
 fe = 256;
-deltaf = 2;
+deltaf = 1;
+G = 1;
 
-K = 1;
 T = importdata('herve001.txt');
 [m,n] = size(T);
 T(1:m,1:n);
-T2 = (T(1:m,1) + T(1:m,2))*0.5;
-T2(1:m)
+signal = (T(1:m,1) + T(1:m,2))*0.5; % Fait la moyenne des deux canaux
+signal(1:m)
 
 t = 0:1/fe:(m-1)*(1/fe);
-figure;
-plot(t,T2)
+figure(1);
+plot(t,signal);
+
+%Paramètres du filtre passe-bande
+b0 = 2*pi*G*deltaf/fe*sin(2*pi*fc/fe);
+a1 = -2*sqrt(1-2*pi*deltaf/fe)*cos(2*pi*fc/fe);
+a2 = 1-2*pi*G*deltaf/fe;
+
+% W1=[5 9]/(fe/2);
+% [a,b]=butter(10,W1,'bandpass'); % On créé le filtre
+% signal_filtered = filter(b,a,signal); % On applique le filtre
+% figure(2),plot(t,signal_filtered);
+
+signal_filtered = filter(b0,[1,a1,a2],signal); % On applique le filtre
+figure(2);
+plot(t,signal_filtered);
+
+figure(3);
+freq = fft(signal);
+plot(abs(freq))
+
--- src/acquisition_biosemi/src/biosemi_listener.cpp
+++ src/acquisition_biosemi/src/biosemi_listener.cpp
@@ -5,13 +5,24 @@
 
 static unsigned long int numero;
 
+class Listener
+{
+public:
+	void biosemiCallback(const acquisition_biosemi::biosemi_result msg);
+	std::vector<double> data;
+	std::vector<double>::iterator pt;
+};
 
 /* Cette fonction est appelée à chaque réception de message */
-void biosemiCallback(const acquisition_biosemi::biosemi_result msg)
+void Listener::biosemiCallback(const acquisition_biosemi::biosemi_result msg)
 {
 	int num, taille;
-	
+
 	taille = msg.valeurs.size();
+	std::cout << "taille " << taille << std::endl ;
+	std::cout << "nombre " << msg.nombre << std::endl;
+	std::cout << "numero " << msg.numero << std::endl;
+
 	std::vector<double>::const_iterator i = msg.valeurs.begin();
 	for(num = 0; num < msg.nombre; num ++ ) {
 		std::cout.setf(std::ios::fixed, std::ios::floatfield);
@@ -21,7 +32,8 @@ void biosemiCallback(const acquisition_biosemi::biosemi_result msg)
 		std::cout.width(0); std::cout.fill(' ');
 		std::cout << "[" << msg.frequence << "]:  ";
 		for(int k = 0; k < taille / msg.nombre; k++) {
-			std::cout << "  " << *i;
+			std::cout << *i;
+			data.push_back(*i);
 			++i;
 		}
 		std::cout << "  " << msg.extra << std::endl;
@@ -35,16 +47,33 @@ void biosemiCallback(const acquisition_biosemi::biosemi_result msg)
 		}
 		numero = msg.numero + num;
 	}
+
+}
+
+
+void display(Listener& listener)
+{
+	std::cout << "NEWWWWW" << std::endl;
+	for(std::vector<double>::iterator i=listener.data.begin();i!=listener.data.end();i++)
+	{
+		std::cout << *i << std::endl;
+	}
+	std::cout << "ENDDDD" << std::endl;
 }
 
 int main(int argc, char **argv)
 {
   numero = 0;
+	Listener listener;
+	listener.pt=listener.data.begin();
+
   ros::init(argc, argv, "biosemi_listener");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("biosemi_result", 1000, biosemiCallback);
+  ros::Subscriber sub = n.subscribe("biosemi_result", 1000, &Listener::biosemiCallback,&listener);
 
   ros::spin();
+
+	display(listener);
 
   return 0;
 }
